f1c410e377d8e7bb51e0d7600c5a59144df7bee4
ina <wenulife@gmail.com>
2022-10-28 18:48:08 +0300

    added pretty print and made a tuple matrix

--- lab_5/main.py
+++ lab_5/main.py
@@ -1,60 +1,58 @@
 import json
 import sys
+from tabulate import tabulate
 
 
 class Model:
-    def __init__(self, players, columns, rows, values):
+    def __init__(self, players, column_names, row_names, values):
         self.players = players
-        self.columns = columns
-        self.rows = rows
+        self.column_names = column_names
+        self.row_names = row_names
         self.values = values
 
-    def not_that_pretty_print(self):
-        json.dump(self.values, sys.stdout, indent=4)
+    def pretty_print(self):
+        table = [[f"{self.players[0]} / {self.players[1]}", self.column_names[0], self.column_names[1]],
+                 [self.row_names[0], self.values[0][0], self.values[0][1]],
+                 [self.row_names[1], self.values[1][0], self.values[1][1]]]
+        print(tabulate(table))
+
 
 class Parser:
     def __init__(self, filename: str):
         self.filename = filename
 
-    def get_lines_no_empty_spaces(self, elements):
+    @staticmethod
+    def get_line_no_empty_spaces(elements):
         return list(filter(lambda x: x != '', elements))
 
-    def parse_line(self, line, players, to_be_filled):
-        line = self.get_lines_no_empty_spaces(line.strip().split(' '))
+    def parse_line(self, line, players):
+        line = self.get_line_no_empty_spaces(line.strip().split(' '))
         players.append(line[0])
-        to_be_filled = line[1:]
-        return to_be_filled
-
-    def make_space_for(self, values, player, line_index):
-        if line_index not in values[player]:
-            values[player][line_index] = dict()
+        return line[1:]
 
     def parse_data_into_model(self):
         players = []
         rows = []
         columns = []
+        values = []
 
-        values = dict()
-        values[0] = dict()
-        values[1] = dict()
         with open(self.filename) as fd:
             lines = fd.readlines()
+            row_names = self.parse_line(lines[0], players)
+            column_names = self.parse_line(lines[1], players)
 
-            self.parse_line(lines[0], players, rows)
-            self.parse_line(lines[1], players, columns)
-            for line_index, line in enumerate(lines[2:], start=0):
-                tuples_data = self.get_lines_no_empty_spaces(line.split(' '))
+            for line_index, line in enumerate(lines[2:]):
+                row = []
+                tuples_data = self.get_line_no_empty_spaces(line.split(' '))
                 for column_index, tuple_data in enumerate(tuples_data):
-                    player_1, player_2 = [int(el) for el in tuple_data.split('/')]
-                    self.make_space_for(values, 0, line_index)
-                    self.make_space_for(values, 1, line_index)
+                    players_values = tuple(int(el) for el in tuple_data.split('/'))
+                    row.append(players_values)
+                values.append(row)
 
-                    values[0][line_index][column_index] = player_1
-                    values[1][line_index][column_index] = player_2
-        return Model(players, columns, rows, values)
+        return Model(players, column_names, row_names, values)
 
 
 if __name__ == '__main__':
     parser = Parser("data.txt")
     model = parser.parse_data_into_model()
-    model.not_that_pretty_print()
+    model.pretty_print()
