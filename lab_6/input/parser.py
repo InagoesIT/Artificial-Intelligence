8c7fb519794c832452b92b3e50fb8de80987f206
ina <wenulife@gmail.com>
2022-11-23 18:03:21 +0200

    forward prop in progress

--- lab_6/input/parser.py
+++ lab_6/input/parser.py
@@ -1,5 +1,4 @@
 from collections import defaultdict
-
 import numpy as np
 
 
@@ -7,7 +6,7 @@ class Parser:
     def __init__(self, filename) -> None:
         self.filename = filename
 
-    def parse(self) -> tuple[list, list]:
+    def parse(self) -> tuple[list, list, list, list]:
         data = defaultdict(lambda: list())
         with open(self.filename) as fd:
             lines = fd.readlines()
@@ -15,15 +14,26 @@ class Parser:
             line = line[:-1]
             attr_values = line.split(",")
             out_value = attr_values[-1]
-            data[out_value].append(attr_values)
+            data[out_value].append(attr_values[:-1])
 
+        # TODO -> FIND A BETTER FIX FOR THIS ISSUE
+        data.pop('')
+
+        train_percentage = 0.8
         train_data = []
+        train_target = []
         test_data = []
+        test_target = []
+
         # 80% training; 20% test
         for item in data.items():
             total_size = len(item[1])
-            train_size = int(total_size * 0.8)
+            train_size = int(total_size * train_percentage)
+            test_size = total_size - train_size
             train_data += item[1][:train_size]
+            train_target += ([item[0]] * train_size)
             test_data += item[1][train_size:]
+            test_target += ([item[0]] * test_size)
 
-        return train_data, test_data
+        return train_data, train_target, test_data, test_target
+
--- lab_6/main.py
+++ lab_6/main.py
@@ -20,7 +20,7 @@ def get_error(results, outputs):
 
 
 def forward_prop(weights, biases, instances):
-    print(instances.dtype)
+    print(type(weights[0]))
     z0 = np.add(weights[0].dot(instances.transpose()), biases[0].reshape(12, 1))
     y0 = sigmoid(z0)
     z1 = np.add(np.dot(weights[1], y0), biases[1].reshape(3, 1))
@@ -33,53 +33,83 @@ def forward_prop_instance(weights, biases, instance):
     y0 = sigmoid(z0)
     z1 = np.add(np.dot(weights[1], y0), biases[1])
     y1 = sigmoid(z1)
-    return y1
+    return z0, y0, z1, y1
 
 
-# TO DO: transform a name into an (3, 1) vector
-# have 2 0's and 1 for the class it belongs to
-# example: IRIS_VERSICOLOR = [0, 1, 0]
-def encode_results(dataset):
-    pass
+def get_one_hot_target(target):
+    IRIS_SETOSA = "Iris-setosa"
+    IRIS_VERSICOLOR = "Iris-versicolor"
+    IRIS_VIRGINICA = "Iris-virginica"
+    IRIS_SETOSA_ARR = [1, 0, 0]
+    IRIS_VERSICOLOR_ARR = [0, 1, 0]
+    IRIS_VIRGINICA_ARR = [0, 0, 1]
+
+    if target == IRIS_SETOSA:
+        return IRIS_SETOSA_ARR
+    elif target == IRIS_VERSICOLOR:
+        return IRIS_VERSICOLOR_ARR
+    elif target == IRIS_VIRGINICA:
+        return IRIS_VIRGINICA_ARR
+    return None
+
+
+def get_one_hot_batch(batch_targets):
+    transformed = []
+
+    for item in batch_targets:
+        transformed.append(get_one_hot_target(item))
+
+    return np.array(transformed)
 
 
 def get_weights_and_biases(hidden_size, input_size=4, output_size=3):
     weights = np.array([np.array([[rand.uniform(-0.1, 0.1) for _ in range(input_size)] for _ in range(hidden_size)]),
-                       np.array([[rand.uniform(-0.1, 0.1) for _ in range(hidden_size)] for _ in range(output_size)])], dtype=object)
+                        np.array([[rand.uniform(-0.1, 0.1) for _ in range(hidden_size)] for _ in range(output_size)])],
+                       dtype=object)
     biases = np.array([np.array([rand.uniform(-0.1, 0.1) for _ in range(hidden_size)]),
-                      np.array([rand.uniform(-0.1, 0.1) for _ in range(output_size)])], dtype=object)
+                       np.array([rand.uniform(-0.1, 0.1) for _ in range(output_size)])], dtype=object)
 
     return weights, biases
 
 
-def train(nr_epochs=25, learning_rate=0.6):
-    pass
+def get_batches(train_data, train_target, batch_size):
+    training_size = len(train_data)
+    batches_data = []
+    batches_target = []
+    for batch_start, batch_end in zip(range(0, training_size, batch_size),
+                                      range(batch_size, training_size, batch_size)):
+        batches_data.append(np.array(train_data[batch_start:batch_end][:]))
+        batches_target.append(np.array(get_one_hot_batch(train_target[batch_start:batch_end])))
 
-def separate_data(train_data):
-    train_data_no_res = []
-    out_data = []
-    for line in train_data:
-        line_arr = line[:-1]
-        out_arr = line[-1]
-        train_data_no_res.append(line_arr)
-        out_data.append(out_arr)
-    return np.array(train_data_no_res), np.array(out_data)
+    return batches_data, batches_target
+
+
+def train(train_data, train_target, weights, biases, nr_epochs=25, learning_rate=0.6, batch_size=20):
+    batches_data, batches_target = get_batches(train_data, train_target, batch_size)
+
+    for epoch_nr in range(nr_epochs):
+        for batch_index in range(len(batches_target)):
+            z0, y0, z1, y1 = forward_prop(weights, biases, batches_data[batch_index])
+            # TODO George will make these 2 work
+            dw1, dw2, db1, db2 = backward_prop(z0, batches_data[batch_index], y0, y1, weights_level_2=weights[1],
+                                               targets=batches_target[batch_index])
+            weights, biases = update_params(weights, biases, dw1, dw2, db1, db2, learning_rate)
+        print(f"epoch = {epoch_nr}")
+        if epoch_nr % 5 == 0:
+            print(f"the accuracy for the training set is: {get_accuracy(weights, biases, training_set)}")
+            print(f"the accuracy for the validation set is: {get_accuracy(weights, biases, validation_set)}")
+    return weights, biases
 
 
 def main():
     data_file = os.path.join("input", "iris.data")
     test_parser = Parser(data_file)
-    train_data, test_data = test_parser.parse()
+    train_data, train_target, test_data, train_target = test_parser.parse()
 
-    # print(train_data)
-    # print(test_data)
-    hidden_size = 12
+    hidden_size = 24
     weights, biases = get_weights_and_biases(hidden_size)
-    # print(weights)
-    # print(biases)
-
-    input_train, target_train = separate_data(train_data)
-    print(forward_prop(weights, biases, input_train))
+    batches_data, batches_target = get_batches(train_data, train_target, 20)
+    z0, y0, z1, y1 = forward_prop(weights, biases, batches_data[0])
 
 
 if __name__ == '__main__':
